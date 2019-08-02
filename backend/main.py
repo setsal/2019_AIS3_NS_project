f823931f3912afa602a8dcf07075a9e1799fd282
setsal Lan <contact@setsal.dev>
2019-08-02 11:51:51 +0800

    link python to the tesseract ocr package

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,14 @@
 # Author: setsal Lan
 
 import sys
+import os
+import subprocess
 import flask
 from flask import render_template, request, redirect
 from werkzeug.utils import secure_filename
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-import os
 
 app.config["IMAGE_UPLOADS"] = "/home/setsal/ais3/project/2019_AIS3_NS_project/backend/img/upload"
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
@@ -24,6 +24,16 @@ def allowed_image(filename):
         return True
     else:
         return False
+
+def image_to_string(img, cleanup=True, plus=''):
+    subprocess.check_output('tesseract -l chi_tra ' + img + ' ' + img + ' ' + plus, shell=True)  #create the image txt
+    text = ''
+    with open(img + '.txt', 'r') as f:
+        text = f.read().strip()
+    if cleanup:
+        os.remove(img + '.txt')
+    return text
+
 
 
 @app.route('/', methods=['GET'])
@@ -42,12 +52,13 @@ def upload():
                 return redirect(request.url)
 
             if allowed_image(image.filename):
-                filename = secure_filename(image.filename)
-                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+                filename = secure_filename( image.filename )
+                image.save( os.path.join(app.config["IMAGE_UPLOADS"], filename) )
                 print("Image saved", file=sys.stderr)
-                return render_template('result.html', result="hello")
+                text = image_to_string( os.path.join(app.config["IMAGE_UPLOADS"], filename ), False )
+                return render_template( 'result.html', result=text )
             else:
-                print("That file extension is not allowed")
+                print( "That file extension is not allowed" )
                 return redirect(request.url)
 
     return render_template('result.html', result="hello2")
